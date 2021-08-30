805a0e783bc9897ab06fe9648a78f061c99fe555
张亚飞 <672233580@qq.com>
2021-08-30 13:32:12 +0800

    Initial Commit

---
+++ PagerTabs0830/PagerTabs0830App.swift
@@ -0,0 +1,17 @@
+//
+//  PagerTabs0830App.swift
+//  PagerTabs0830
+//
+//  Created by 张亚飞 on 2021/8/30.
+//
+
+import SwiftUI
+
+@main
+struct PagerTabs0830App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
